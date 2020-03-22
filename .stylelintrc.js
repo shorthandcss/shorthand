ab35c8e70a79107d0a0b0dd5e83f0726dfb5821a
kimulaco <kmrkdev@gmail.com>
2020-03-23 01:21:53 +0900

    feat: disabled no-descending-specificity

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: 'stylelint-config-standard',
     plugins: ['stylelint-scss'],
     rules: {
+        'no-descending-specificity': null,
         'at-rule-no-unknown': null,
         'scss/at-rule-no-unknown': true
     }
